914acdb7c9df154e8c6f9a32b5e46cd0f1ebb305
Alex D'Andrea <alex@dandrea.io>
2024-10-26 16:22:32 +0200

    read first swaymsg events

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,25 @@
-console.log('Hello World');
+import child_process from 'child_process';
+
+async function main() {
+  console.log('Spawning swaymsg...');
+  const spawn = child_process.spawn('swaymsg', ['--monitor', '--type', 'subscribe', '["window"]']);
+
+  spawn.stdout.on('data', (chunk: any) => {
+    if (chunk instanceof Buffer) {
+      try {
+        const data = JSON.parse(chunk.toString());
+        console.log(data);
+      } catch (err: unknown) {
+        console.error(err);
+      }
+    }
+  });
+
+  spawn.stderr.on('data', (chunk: any) => {
+    console.error(chunk);
+  });
+
+  return spawn;
+}
+
+await main();
